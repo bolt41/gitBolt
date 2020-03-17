d3a9c18f0180edeb3e459051a85311249c3517ef
Кирилл <bolt@pop-os.localdomain>
2020-03-17 15:32:18 +0300

    Добавлена мультизадачность

--- parsing_with_ajax/pars_with_ajax.py
+++ parsing_with_ajax/pars_with_ajax.py
@@ -1,30 +1,39 @@
 import csv
+from multiprocessing import Pool #импорт модуля мультизадачности
 import requests
 
 
-def get_html(url):
+def get_html(url): #функция получения кода страницы
     r = requests.get(url)
     return r.text
 
 
-def write_csv(data):
-    field = ['name', 'url', 'rate']
+def write_csv(data): #функция записи данных в csv файл
+    field = ['name', 'url', 'rate'] #определяем последовательность полей для записи
     with open('liveinternet.csv', 'a') as f:
         writer = csv.DictWriter(f, fieldnames=field)
         writer.writerow(data)
 
+def gate_page_data(text): #функция обработки полученного текста и подготовка его к записи
+    response = text.strip().split('\n')[1:] # удаляем лишние пробелы и разделяем строки по символу переноса
+    for row in response: # бежим циклом по получившемуся списку
+        columns = row.strip().split('\t') #текущую строку разбиваем по табуляции
+        data = {'name': columns[0], #записываем необходимые значения из получившегося списка в словарь
+                'url': columns[1],
+                'rate': columns[3]}
+        write_csv(data) #отправляем словарь на запись
+
+def make_all(url): #функция-посредник получает текст из запроса текущего
+    text = get_html(url)
+    gate_page_data(text)
+
 
 def main():
-    for i in range(1, 6761):
-        url = f'https://www.liveinternet.ru/rating/ru//today.tsv?page={i}'
-        response = get_html(url).strip().split('\n')[1:]
-        for row in response:
-            columns = row.strip().split('\t')
-            data = {'name': columns[0],
-                    'url': columns[1],
-                    'rate': columns[3]}
-            write_csv(data)
+    #6761
+    urls = [f'https://www.liveinternet.ru/rating/ru//today.tsv?page={i}' for i in range(1, 6762)]
 
+    with Pool(20) as p: #устанавливаем количсетво процессов = 20
+        p.map(make_all, urls) #запускаем функцию по значениям списка
 
 if __name__ == '__main__':
     main()
