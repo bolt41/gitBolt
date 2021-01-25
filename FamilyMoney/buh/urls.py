f865c098d73fb4a6d921836f1b7dd624260b0afe
bolt41 <bolt1986@bk.ru>
2021-01-25 16:48:18 +0300

    Update FMoney

--- FamilyMoney/buh/urls.py
+++ FamilyMoney/buh/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import HomePageView, AddTransaction
 
 urlpatterns = [
-    path("",),
+    path('', HomePageView, name='home'),
+    path('add', AddTransaction, name='add_trans')
 
 ]
--- FamilyMoney/buh/views.py
+++ FamilyMoney/buh/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import OverFunds, TransType, Trans, SourceMoney, Users
+from django.db.models import F, Sum
 
-# Create your views here.
+
+def HomePageView(request):
+    sources = SourceMoney.objects.all()
+    total = SourceMoney.objects.aggregate(Sum('sum'))
+
+    return render(request, 'index.html', {
+        'sources': sources,
+        'total': total.get('sum__sum')
+    })
+
+def AddTransaction(request):
+    sources = Trans.objects.all()
+
+    return render(request, 'add_trans.html', {
+        'sources': sources
+
+    })
+
+
+
